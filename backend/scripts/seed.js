13e629717c5580f26a8bbe2031580f3542aa5448
cbraian42 <127146446+cbraian42@users.noreply.github.com>
2025-10-29 15:46:51 -0300

    scripts seed y clear DB

--- backend/scripts/seed.js
+++ backend/scripts/seed.js
@@ -1,21 +1,18 @@
+// scripts/seed.js
 import dotenv from 'dotenv';
 dotenv.config();
 
 import mongoose from 'mongoose';
-import Product from './models/Product.js'; // 👈 Agregar .js
+import Product from '../models/Product.js'; 
 import fs from 'fs';
 
 async function main() {
   await mongoose.connect(process.env.MONGODB_URI);
   console.log('✅ Conectado a MongoDB para seed');
 
-  // Leer archivo JSON
-  const data = JSON.parse(fs.readFileSync('./data/productos.json', 'utf-8'));
+  // Ruta del JSON
+  const data = JSON.parse(fs.readFileSync('../data/productos.json', 'utf-8'));
 
-  // Opción: limpiar colección antes (solo si querés empezar limpio)
-  // await Product.deleteMany({});
-
-  // Insertar todos los productos
   const inserted = await Product.insertMany(data);
   console.log(`✅ ${inserted.length} productos cargados correctamente`);
 
