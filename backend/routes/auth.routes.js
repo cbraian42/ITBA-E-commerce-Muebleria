ca70260cbc8cfe0be52b4e42997e4f926d2e7dbf
cbraian42 <cbraian2000@outlook.com>
2025-11-27 15:41:34 -0300

    auth.routes usa el register

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,7 +1,8 @@
 import express from "express";
+import { register } from "../controllers/auth.controller.js";
 const router = express.Router();
 
-router.post("/register", (req, res) => { });
+router.post("/register", register);
 router.post("/login", (req, res) => { });
 
 export default router;
