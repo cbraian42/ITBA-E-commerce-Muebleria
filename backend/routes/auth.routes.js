f7c27800e5282d0f873b00fdbc78e68c898df8b9
cbraian42 <127146446+cbraian42@users.noreply.github.com>
2025-11-28 09:55:40 -0300

    middleware autenticacion - exitoso

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,8 +1,15 @@
 import express from "express";
 import { register, login } from "../controllers/auth.controller.js";
+import { verifyToken, verifyAdmin } from "../middleware/auth.middleware.js";
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.get("/profile", verifyToken, async (req, res) => {
+  return res.json({
+    message: "Perfil del usuario autenticado",
+    user: req.user
+  });
+});
 
 export default router;
