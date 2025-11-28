f38263dd656039fbc5cffb09d7c03d7d7edf2df4
Augusto Almarcha <augustoalmarcha@gmail.com>
2025-11-28 10:35:15 -0300

    Le agrego estilo al Login

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react'
 import { AuthContext } from '../auth/AuthContext';
+import './ContactForm.css'; //Usamos el mismo estilo que en contacto.
 
 function Login() {
     const { login } = useContext(AuthContext);
@@ -23,21 +24,37 @@ function Login() {
             alert(`Error en el login: ${error.message}`);
         }
     };
-    return (
-        <div>
-            <h1>Inicio de sesion</h1>
-            <form>
-                <div>
-                    <label>Usuario:</label>
-                    <input></input>
-                </div>
-                <div>
-                    <label>Contraseña:</label>
-                    <input></input>
-                </div>
-            </form>
+return (
+  <div className="contact-page">
+    <div className="contact-box">
+      <form className="contact-form">
+        <h1>Inicio de sesión</h1>
+
+        <div className="form-group">
+          <label>Usuario:</label>
+          <input
+            type="text"
+            placeholder="Tu usuario"
+            className="input-field"
+          />
         </div>
-    )
+
+        <div className="form-group">
+          <label>Contraseña:</label>
+          <input
+            type="password"
+            placeholder="Tu contraseña"
+            className="input-field"
+          />
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Iniciar sesión
+        </button>
+      </form>
+    </div>
+  </div>
+)
 }
 
 export default Login
